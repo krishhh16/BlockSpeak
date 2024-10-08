8629b0e33e1fa464522fe22b4d486af3f3e56712
Zeref <krishh16@yahoo.com>
2024-10-09 02:21:22 +0530

    added a few more features to the frontend

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,22 @@
 
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import axios from "axios";
 import { useState } from "react";
+import {toast, Toaster} from 'sonner'
+
 
 export default function Home() {
+
+  const handleSearch = async () => {
+    toast("Fetching the transactions");
+    const response = await axios.post("/api/search", {query});
+    if (response.data.success) {
+      toast.success("Fetched the transactions");
+    } else {
+      toast.error("Fetch couldn't happen");
+    }
+  }
 
   const {publicKey} = useWallet();
   const [query, setQuery] = useState('');
@@ -14,7 +27,7 @@ export default function Home() {
       <h1 className="text-2xl font-bold">BlockSpeak</h1>
       <WalletMultiButton style={{}} />
     </div>
-    <input type='text' disabled={!publicKey} onChange={(e) => setQuery(e.target.value)} value={query} className="p-2 text-black rounded-md" placeholder='Enter your query for the transaction' />
+    <input type='text' disabled={!publicKey} onChange={(e) => setQuery(e.target.value)} value={query} className="p-2 mb-4 text-black rounded-md" placeholder='Enter your query for the transaction' />
     <button disabled={!publicKey} className="p-2 rounded-md bg-blue-500 text-white">Search</button>
    </div>
   );
